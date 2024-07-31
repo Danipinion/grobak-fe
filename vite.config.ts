0db69d4808bd9e3b69d723984847fc1f755fb53b
naufinur <khayatinaufi23gmail.com>
2024-07-31 11:24:13 +0700

    ⚙:fix route

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, ServerOptions } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
+interface CustomServerOptions extends ServerOptions {
+  setup?: (app: any) => void;
+}
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: true,
+    setup: (app) => {
+      app.use((req: any, res: any, next: any) => {
+        res.setHeader("ngrok-skip-browser-warning", "true");
+        next();
+      });
+    },
+  } as CustomServerOptions,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
